15b1966db321f7953393a06aedec4b11556b36e0
Jon Marx <jon.marx@me.com>
2014-09-19 03:06:48 +0000

    added post delete functionality to blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,13 @@ class PostsController < ApplicationController
     redirect_to @post
   end
   
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to "/posts"
+  end
+  
   private
   def post_params
     params.require(:post).permit(:title, :text)
